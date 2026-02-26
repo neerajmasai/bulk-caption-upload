6db41c23e4238a7f1cd96007db8f3a82387f3bcb
Neeraj Malwal <neerajmalwal10@gmail.com>
2026-02-26 17:39:03 +0530

    Refactor code structure for improved readability and maintainability

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'f875d586959c44bc7cf6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '4abb1de4aba54ea0c899');
