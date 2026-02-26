7ce6b0ee071cf0b6f1fa13a96072bda216de31b6
Neeraj Malwal <neerajmalwal10@gmail.com>
2026-02-26 19:51:55 +0530

    Add custom toolbar icon for bulk upload captions feature

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '4abb1de4aba54ea0c899');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'd755e542b974e8fb2b74');
